99d25795f7203d80f151cc83f1fd3851a6702661
Racha <br_bia@esi.dz>
2015-10-10 12:00:20 -0700

    SECOND COMMIT : button comment not working

--- micro-blog/base.js
+++ micro-blog/base.js
@@ -2,6 +2,63 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+
+	var posting = new PostsHandler();
+	posting.addListeners();
+	
 
 });
+
+var ids=0;
+var content; // a content
+var p; // a post
+var div; // the div that will be added after every click
+var name; // the name of the poster
+var posts=[];
+
+function Post(name,id, content){
+	this.name=name;
+	this.id = id;
+	this.content = content;
+	this.comment = [];
+	this.addComment= function(com){
+		this.comment.push(com);
+	}
+	
+}
+
+function PostsHandler() {
+	this.posts= [];
+	console.log ('this.posts = ' + this.posts);
+	this.addListeners= function(){
+		$('#button-Post').on('click', function(){
+			ids++;
+			debugger;
+			content = $('.text-Input').val();
+		    name    = $('.name-Input').val();
+		    p = new Post(name,ids,content);
+		    console.log('the post is' + p);
+		    console.log('this is '+ this);
+		    console.log ('this.posts = ' + this.posts);
+		    posts.push(p);
+		    addPost(ids,name,content);
+		});
+		$('#button-com').on('click', function(){
+			alert('clicked');
+
+		});
+	};
+}
+
+function addPost(ids,name,content){
+		var div ='<div id= \" '+ ids + '\"'+'class = "col-lg-3 col-sm-3 box1" style ="display:none"> <h1 id="hOne">'
+		+ name + '</h1><p>'
+		+ content 
+		+ '</p><br/><a href="#" id="button-com" class="btn btn-danger btn-lg btn-huge lato" data-toggle="modal" data-target="#myModal"> Comment</a> ';
+		$('.row1').prepend(div);
+		$(".col-sm-3").slideDown('slow');
+		$('.text-Input').val('');
+		$('.name-Input').val('');
+	};
+
+
